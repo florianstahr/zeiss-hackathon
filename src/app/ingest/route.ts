ec20d539f30746d9ac2fe55b1c3bb53b0d3c14e7
Florian Stahr <mail@florianstahr.com>
2024-06-04 12:31:15 +0200

    Add insert to mongo

--- src/app/ingest/route.ts
+++ src/app/ingest/route.ts
@@ -1,8 +1,15 @@
 import { NextRequest } from 'next/server';
+import { MongoClient } from 'mongodb';
+
+const client = new MongoClient(process.env.MONGO_URI as string);
 
 export async function POST(req: NextRequest) {
   const data = await req.json()
-  console.log('received', data);
+  // console.log('received', data);
 
-  return Response.json({ lol: true })
+  const db = client.db('zeiss_hackathon');
+
+  await db.collection('data').insertOne(data);
+
+  return Response.json({ lol: true });
 }
